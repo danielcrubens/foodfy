5de67f60ecc9887c610085dd5a3ee0949f27e3db
danielcrubens <dcr.tecnologi@gmail.com>
2022-05-16 19:54:25 -0300

    VALIDA  :construction:

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -270,3 +270,37 @@ const Lightbox = {
     Lightbox.closeButton.style.top = '-80px';
   }
 };
+
+//VALIDAÇÃO FORMULÁRIO
+const Validate = {
+  apply(input, func) {
+      Validate.clearErrors(input);
+      let results = Validate[func](input.value);
+
+      if (results.error) Validate.displayError(input, results.error);
+
+  },
+  displayError(input, error) {
+      const div = document.createElement('div');
+      div.classList.add('error');
+      div.innerText = error;
+      input.parentNode.appendChild(div);
+
+      input.focus();
+  },
+  clearErrors(input) {
+      const errorDiv = input.parentNode.querySelector('.error');
+      if (errorDiv) errorDiv.remove();
+  },
+  isEmail(value) {
+      let error = null;
+      const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+      if(!value.match(mailFormat)) error = 'Email inválido';
+
+      return {
+          error,
+          value
+      };
+  }
+}
