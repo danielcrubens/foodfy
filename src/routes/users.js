d3a3496080f6e30fc54a8182f52b203c99e64427
danielcrubens <dcr.tecnologi@gmail.com>
2022-05-18 12:05:34 -0300

    página reset password  :construction:

--- src/routes/users.js
+++ src/routes/users.js
@@ -13,6 +13,9 @@ routes.get('/login', SessionController.loginForm);
 routes.post('/login', SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout);
 
+// Reset Password //
+routes.get('/forgot-password', SessionController.forgotForm);
+routes.get('/password-reset', SessionController.resetForm);
 
 // User register //
 routes.get('/register', UserController.registerForm);
